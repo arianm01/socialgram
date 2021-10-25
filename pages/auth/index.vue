01bec5f98af234e9fec24830db10cf9331810e89
arianm01 <arian.ahmadi1379@gmail.com>
2021-10-25 15:53:17 +0330

    resizing the page works

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -1,13 +1,63 @@
 <template>
-<h1>Auth</h1>
+  <div class="left-side">
+    <img src="../../assets/logo/500ppi/Logo3.png" width="100">
+    <div class="left-side-call" v-if="sizeScreen">
+    <h1>Call a friend,</h1>
+      <div class="call">
+        <h1>Call</h1>
+        <img src="../../assets/logo/500ppi/Logo5.png" class="logo-call">
+      </div>
+     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "index"
+  name: "index",
+  data() {
+    return {
+      size: null,
+      sizeScreen:true,
+    }
+  },
+
+  watch: {
+    sizeScreen(){
+      console.log(this.size > 500);
+      return this.size > 500;
+    }
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.onResize);
+    })
+  },
+
+  methods: {
+    onResize() {
+      this.size = window.innerWidth;
+      this.sizeScreen = this.size > 500;
+    }
+  }
 };
 </script>
 
 <style scoped>
-
+.left-side{
+  font-size: 30px;
+  padding: 3%;
+  background-color: aquamarine;
+}
+.left-side-call{
+  padding-top: 15%;
+  margin-left: 10%;
+}
+.logo-call{
+  width: 100px;
+  padding-left: 1%;
+}
+.call{
+  display: flex;
+}
 </style>
