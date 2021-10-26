5068530ba3460ce45d5b7545796417efdde6ce36
arianm01 <arian.ahmadi1379@gmail.com>
2021-10-26 12:01:30 +0330

    some improvement in responsive area

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -2,15 +2,15 @@
   <div class="main h-screen w-full w-screen">
     <div class="grid left-side w-1/3 ">
       <img src="../../assets/logo/500ppi/Logo3.png" width="100">
-      <div class="left-side-call justify-self-center" v-if="sizeScreen">
+      <div class="left-side-call justify-self-center " v-if="sizeScreen">
         <h1>Call a friend,</h1>
         <div class="call">
           <h1>Call</h1>
           <img src="../../assets/logo/500ppi/Logo5.png" class="logo-call">
         </div>
       </div>
-      <div class="left-side-image">
-        <!--      some image-->
+      <div class="left-side-image mt-10">
+        <img src="../../assets/pics/Saly-10(1).png" alt="saly">
       </div>
     </div>
     <div class="right-side w-2/3">
@@ -64,10 +64,20 @@ export default {
   background-color: #b0d8da;
 }
 
+.left-side img{
+  /*margin-bottom: 30%;*/
+}
+.left-side-image{
+  margin-top: -50px;
+  padding: 10%;
+  top: -50px;
+}
+
 .left-side-call {
   /*padding-top: 15%;*/
   /*margin-left: 10%;*/
-  margin-bottom: 10%;
+  padding-top: 20%;
+  margin-bottom: 0;
 }
 
 .logo-call {
@@ -77,6 +87,11 @@ export default {
 
 .call {
   display: flex;
-  right: 10%;
+}
+@media only screen and (max-width: 750px) {
+  /* For mobile phones: */
+  .main{
+    display: grid;
+  }
 }
 </style>
